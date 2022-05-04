30156677285a8d07ce827b28662755a824225703
xfnw <xfnw@riseup.net>
2022-05-03 20:53:42 -0400

    add some html to the example

--- example.php
+++ example.php
@@ -5,7 +5,10 @@ $url = "http://feesh:7700/indexes/movies/search";
 $title = 'movie search';
 
 function handleItem($row) {
-print_r($row);
+	echo "<img src='$row->poster' height='150px' width='100px'>";
+	echo "<h2>$row->title</h2>";
+	echo $row->overview;
+	echo '<div class=clear></div>';
 }
 
 
